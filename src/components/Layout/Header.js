45710e008367801b75c7ffde08ad7e02b676e242
Daniel Rosenthal <daniel.b.rosenthal@gmail.com>
2023-01-24 19:51:34 -0500

    first edits

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react"
+
+const Header = (props) => {
+    return (
+        <Fragment>
+            <div>Daniel's React Website</div>
+        </Fragment>
+    )
+}
+
+export default Header;
