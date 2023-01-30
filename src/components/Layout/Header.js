9824c4e340d9248a56f75f6ab4c7daee62314b91
Daniel Rosenthal <daniel.b.rosenthal@gmail.com>
2023-01-30 17:57:06 -0500

    messing around, giving up for now

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,9 +1,14 @@
 import { Fragment } from "react"
+import classes from './Header.module.css'
+
 
 const Header = (props) => {
     return (
         <Fragment>
             <div>Daniel's React Website</div>
+            <header className={classes.header}>
+            
+            </header>
         </Fragment>
     )
 }
---
+++ src/components/Layout/Navigation.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+const Naviagtion = (props) => {
+    return (
+        <Fragment>
+
+        </Fragment>
+    );
+}
+
+export default Navigation;
