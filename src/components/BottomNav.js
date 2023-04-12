11200eaab9de0c87bad31661e1b3055b37b7114a
Daniel Rosenthal <daniel.b.rosenthal@gmail.com>
2023-04-11 22:39:25 -0400

    edits

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -1,9 +1,24 @@
 import React, {Fragment} from "react";
-
+import styles from './BottomNav.module.css';
+import { Link, useLocation } from 'react-router-dom';
 const BottomNav = () => {
+    const location = useLocation();
     return (
         <Fragment>
-
+            <div className={styles.content}>
+                <div className={styles.pages}>
+                <Link 
+                to="/" 
+                className={location.pathname === '/' ? `${styles.link} ${styles.active}` : styles.link}>
+                Home
+                </Link>
+                {/* <Link 
+                to="/about" 
+                className={location.pathname === '/about' ? `${styles.link} ${styles.active}` : styles.link}>
+                Contact
+                </Link> */}
+                </div>
+            </div>
         </Fragment>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,21 @@ import uzi from './Assets/uzi.png';
 
 const Home = () => {
     return (
-        <Fragment>
-            <div className={styles.content}>
-                <p className={styles.intro}>Hey! 👋 I'm Daniel and this is my website. I am a college student and this website is a collection of my work and hobbies.</p>
-                 
-                <img className={styles.uzi} src={uzi} alt="logo"/>
-            </div>
-        </Fragment>
-    )
+      <Fragment>
+        <div className={styles.content}>
+          <img className={styles.uzi} src={uzi} alt="logo" />
+          <p className={styles.intro}>
+            Hey! 👋 I'm <span className={styles.underline}>Daniel</span> and this is my website. I am a college student
+            and this website is a collection of my work and hobbies. 
+          </p>
+          <p className={styles.intro2}>
+          I am actively pursuing
+            jobs in <span className={styles.underline}>software engineering</span>, <span className={styles.underline}>data analytics</span>, and various other engineering roles.
+            Please feel free to look around and contact me with any questions.
+          </p>
+        </div>
+      </Fragment>
+    );
 }
 
 export default Home;
