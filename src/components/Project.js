188c481e4a2137f75b40675d31758c3124746b0d
Daniel Rosenthal <daniel.b.rosenthal@gmail.com>
2023-05-31 21:34:03 -0400

    lots of stuff

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,14 +1,35 @@
-import React, {Fragment} from "react";
-import styles from './Project.module.css';
+import React, { Fragment } from "react";
+import styles from "./Project.module.css";
+import FadeInOnScroll from "./UI/FadeInOnScroll"
 const Project = (props) => {
-    return (
-        <Fragment>
-            <div className={styles.content}>
-                <h2 className={styles.title}>{props.title}</h2>
-                <p className={styles.description}>{props.description}</p>
-            </div>
-        </Fragment>
-    )
-}
+  return (
+    <Fragment>
+        <FadeInOnScroll>
+        <div className={styles.content}>
+        <h2 className={styles.title}>{props.title}</h2>
+        <ul className={styles.description}>
+          {props.description.map((bullet, index) => (
+            <li key={index} dangerouslySetInnerHTML={{ __html: bullet }}>
+              
+            </li>
+          ))}
+        </ul>
+        {Array.isArray(props.images) &&  (<div className={styles.images}>
+          {props.images.map((image, index) => (
+            <img key={index} src={image} alt={props.title} />
+          ))}
+        </div>)}
+
+        {props.video && (
+          <video controls className={styles.video}>
+            <source alt={props.title} src={props.video}></source>
+          </video>
+        )}
+      </div>
+        </FadeInOnScroll>
+      
+    </Fragment>
+  );
+};
 
 export default Project;
